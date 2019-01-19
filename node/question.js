7282514560207796cbb20f8046b67af2fb1a5485
sproulel <sproulelucas13@gmail.com>
2019-01-19 10:42:38 -0500

    node changes

--- node/question.js
+++ node/question.js
@@ -1,9 +1,19 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
+var rl = require('readline');
+var input = rl.createInterface(process.stdin, process.stdout)
 
-readline.question(`What's your name? `, (name) => {
-  console.log(`Hi ${name}!`)
-  readline.close()
+
+var Person = {
+  name : '',
+  age : 0
+}
+
+
+input.question(`what is your name? `, function(name){
+  Person.name=name
+  console.log(`Hi ${Person.name}`)
+  input.setPrompt(`How old are you`)
+  Person.age=input.prompt()
+
+  console.log(`you are ${Person.age}`)
+  //input.close()
 })
