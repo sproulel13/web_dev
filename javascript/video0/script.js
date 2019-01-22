4cc9aa088a6eb56f41cb7920d0c4e641973846bb
Ann Leoanrd <aleo0779@gmail.com>
2019-01-21 23:09:07 -0500

    updates for github

---
+++ javascript/video0/script.js
@@ -0,0 +1,17 @@
+//console.log(this);
+var age=this.prompt("how old are you");
+
+
+if (parseInt(x) > 17){
+  console.log("you can smoke cancer sticks")
+}
+
+if (parseInt(x) > 21){
+  console.log("you can drink")
+}
+
+var state=this.prompt("where do you live")
+
+if (state == "CA" && parseInt(x) > 20){
+  console.log("puff puff")
+}
